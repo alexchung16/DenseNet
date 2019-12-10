3fa401c4c449c8dea81450ff283eb14bd2dc02e1
alexchung <yonganzhong@outlook.com>
2019-12-10 16:09:46 +0800

    update code

--- DenseNet_Demo.py
+++ DenseNet_Demo.py
@@ -10,6 +10,7 @@ import os
 import cv2 as cv
 import matplotlib.pyplot as plt
 import tensorflow as tf
+from tensorflow.python import pywrap_tensorflow
 
 import numpy as np
 from TFRecordProcessing.parse_TFRecord import reader_tfrecord, get_num_samples
@@ -33,7 +34,26 @@ if __name__ == "__main__":
         # sess.run(init)
         # print(sess.run(w1))
 
-        new_saver = tf.train.import_meta_graph(os.path.join(model_path, 'tf-densenet121.ckpt.meta'))
-        new_saver.restore(sess, save_path=pretrain_model_dir)
-        for var in tf.model_variables():
-            print(var.name)
+        tf.train.import_meta_graph(os.path.join(model_path, 'tf-densenet121.ckpt.meta'))
+        # new_saver.restore(sess, save_path=pretrain_model_dir)
+
+        graph = tf.get_default_graph()  # 获得默认的图
+        input_graph_def = graph.as_graph_def()  # 返回一个序列化的图代表当前的图
+
+        for node in input_graph_def.node:
+            print(node.name, node.op)
+
+        # for var in tf.model_variables():
+        #     print(var.name)
+
+
+        # x = graph.get_operation_by_name('input')
+        # y = graph.get_operation_by_name('logits')
+
+        # reader = pywrap_tensorflow.NewCheckpointReader(pretrain_model_dir)
+        # var_to_shape_map = reader.get_variable_to_shape_map()
+        # for key in var_to_shape_map:
+        #     print('tensor_name: ', key)
+
+        # for var in tf.model_variables():
+        #     print(var.name, var.shape)
